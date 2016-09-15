f3d5972e9b2fd7d4ea849662888cdbcc04da2ffe
Sean Wilson <hello@imsean.me>
2016-09-14 22:37:16 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# lmao.rip [![Build Status](https://travis-ci.org/seanc/lmao.rip.svg?branch=master)](https://travis-ci.org/seanc/lmao.rip)
+
+> Generate random lmao gifs
+
+## Usage
+
+```sh
+$ git clone
+$ cd lmao.rip
+$ npm install
+$ node .
+```
+
+## License
+
+MIT © [Sean Wilson](https://imsean.me)
---
+++ index.js
@@ -0,0 +1,59 @@
+var config = require('rc')('lmao', {
+  key: '',
+  port: 3000
+});
+var express = require('express');
+var app = express();
+var request = require('request');
+
+app.get('/', function(req, res) {
+  request(`http://api.giphy.com/v1/gifs/random?api_key=${config.key}&tag=lmao`,
+  function(err, resp, body) {
+    if (err || resp.statusCode !== 200) {
+      return res.send('An error occurred');
+    }
+    var data = JSON.parse(body);
+    var image = data.data.image_url;
+    res.send(`
+      <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+            <title>lmao.rip</title>
+            <style>
+              body {
+                background-color: #000;
+              }
+              img {
+                position: absolute;
+                top: 50%;
+                left: 0;
+                right: 0;
+                margin: auto;
+                transform: translateY(-50%);
+              }
+              .footer {
+                position: absolute;
+                bottom: 15px;
+                color: #fff;
+                opacity: 0.4;
+                font-size: 12px;
+                font-family: 'Arial';
+              }
+            </style>
+          </head>
+          <body>
+            <img src="${image}"/>
+
+            <p class="footer">Powered by Giphy</p>
+          </body>
+        </html>
+      `);
+  });
+});
+
+app.listen(config.port, function() {
+  console.log('lmao.rip started');
+});
